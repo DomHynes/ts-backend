4c49cde1d0a7793477b5037584723630da6ce4e4
domhynes <dominichynes@gmail.com>
2019-06-01 17:11:06 +1000

    [feat] added log outputs incl. slack

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,8 @@
 export default {
-  jwtSecret: process.env.jwt_secret || 'supersecretssh',
+  jwtSecret: process.env.JWT_SECRET || 'supersecretssh',
+  slack: {
+    token: process.env.SLACK_TOKEN,
+    alertsChannel: process.env.SLACK_ALERTS_CHANNEL,
+    username: process.env.SLACK_ALERTS_USERNAME,
+  },
 };
--- src/logging/index.ts
+++ src/logging/index.ts
@@ -1,5 +1,21 @@
 import * as log4js from 'log4js';
+import config from '../config';
 
-const logger = log4js.getLogger();
-
-logger.level = 'debug';
+log4js.configure({
+  appenders: {
+    default: {
+      type: 'console',
+    },
+    slackAlerts: {
+      type: '@log4js-node/slack',
+      token: config.slack.token,
+      // eslint-disable-next-line @typescript-eslint/camelcase
+      channel_id: config.slack.alertsChannel,
+      username: config.slack.username,
+    },
+  },
+  categories: {
+    default: { appenders: ['default'], level: 'debug' },
+    slack: { appenders: ['slackAlerts'], level: 'error' },
+  },
+});
